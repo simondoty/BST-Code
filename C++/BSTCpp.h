ba7bf670ded6c3433f5550f0d720c2011ad4164e
dotysn <simon.doty@gmail.com>
2013-01-13 18:13:13 -0600

    remove not quite working still

--- C++/BSTCpp.h
+++ C++/BSTCpp.h
@@ -5,6 +5,8 @@
  */
 #include <cstddef>
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 // templateed node class
 template< typename T>
@@ -13,24 +15,48 @@ class BSTNode
 
 	public:
 
+		/*
+		 Constructor taking one argument for value
+		 @param info:
+		 		initial value of node
+		 */
 		BSTNode (T info) : _myInfo (info), _myRight (NULL), _myLeft (NULL) {}
 		
+		/*
+		 getInfo returns the value stored in the node
+		 */
 		T getInfo() { 
 			return _myInfo;
 		}
 
+		/*
+		 getLeft returns the left child node
+		 */
 		BSTNode<T>* getLeft() {
 			return _myLeft; // replace
 		}
 
+		/*
+		 getRight returns the right child node
+		 */
 		BSTNode<T>* getRight() {
 			return  _myRight; // replace
 		}
 
+		/*
+		 Sets the right child node
+		 @param newRight:
+		 		the new right child node
+		 */
 		void setRight(BSTNode<T>* newRight) {
 			_myRight = newRight;
 		}
 
+		/*
+		 Sets the left child node
+		 @param newLeft:
+		 		the new left child node
+		 */
 		void setLeft(BSTNode<T>* newLeft) {
 			_myLeft = newLeft;
 		}
@@ -39,8 +65,8 @@ class BSTNode
 			_myInfo = info;
 		}
 
-	//private:
-		// private data members
+	    // data members
+
 		BSTNode<T>* _myRight;
 		BSTNode<T>* _myLeft;
 		T _myInfo;
@@ -66,9 +92,21 @@ public:
  	  Destructor to delete values on heap
  	 */
  	~BSTCpp() {
- 		delete _root->_myLeft;
- 		delete _root->_myRight;
- 		delete _root;
+ 		recursiveDelete(_root);
+ 		//delete _root;
+ 	}
+
+ 	/*
+ 	  recursiveDelete helper to delete allocs
+ 	  @param node:
+ 	  		current node
+ 	 */
+ 	void recursiveDelete(BSTNode<T>* node) {
+ 		if (node != NULL) {
+ 			recursiveDelete(node->getLeft());
+ 			recursiveDelete(node->getRight());
+ 			delete node;
+ 		} 			
  	}
 
 	/*
@@ -79,7 +117,6 @@ public:
 	void insert(T info) {
 		// use iterative algo
 		BSTNode<T>* temp = _root;
-		BSTNode<T>* newNode;
 		bool insert_node = false;
 
 		while (!insert_node) {
@@ -88,22 +125,19 @@ public:
 				if (temp->getLeft() == NULL) {
 					temp->_myLeft = new BSTNode<T>(info);
 					insert_node = true;
-				} 
-				else 
+				} else 
 					temp = temp->getLeft();
 			}
 			else if (info > temp->getInfo()) {
 				if (temp->getRight() == NULL) {
 					temp->_myRight = new BSTNode<T>(info);					
 					insert_node = true;
-				}  
-				else 
+				} else 
 					temp = temp->getRight();
-			} else
+			} 
+			else
 				std::cout << "Value to add is already present." << std::endl;
-		}
-		//if (newNode != 0)
-			//delete newNode;
+		}		
 	}   
 	
 	/*
@@ -112,9 +146,55 @@ public:
 	 	value to remove from tree if present
 	 */
 	void remove(T info) {
-
+		_root = recursiveRemove(_root, info);
 	}
 
+	/*
+	  Recursive helper for remove method
+	  @param node:
+	  		current node to be evaluated
+	  @param info:
+	  		value to be removed if found
+	 */
+	BSTNode<T>* recursiveRemove(BSTNode<T>* node, T info) {
+		// base case
+		if (node == NULL)
+			return NULL;
+		T tempInfo = node->getInfo();
+		if (info < tempInfo)
+			node->setLeft(recursiveRemove(node->getLeft(), info));
+		else if (info > tempInfo)
+			node->setRight(recursiveRemove(node->getRight(), info));
+		else {
+			_size -= 1;
+			if (node->getRight() == NULL && node->getLeft() == NULL)
+				delete node; //node = NULL;
+			else if (node->getRight() == NULL)
+				node = node->getLeft();
+			else if (node->getLeft() == NULL)
+				node = node->getRight();
+			else {
+				node->setInfo(max(node->getLeft()));
+				node->setLeft(recursiveRemove(node->getLeft(), node->getInfo()));
+				_size += 1;
+			}
+		}
+
+		return node;
+	}
+
+	/*
+	 Return max value given starting node.
+	 @param tempNode:
+	 		starting node
+	 */
+	T max(BSTNode<T>* tempNode) {
+		if (tempNode == NULL)
+			std::cout << "Invalid parameter domain" << std::endl;
+		while (tempNode->getRight() != NULL)
+			tempNode = tempNode->getRight();
+		return tempNode->getInfo();
+	}
 	/*
 	  determine if giving value is present in tree or not
 	  @param:
@@ -136,12 +216,19 @@ public:
 		return false; 
 	}
 
-	/*
-	  print tree 
-	 */
-	void print_tree() {
 
-	}
+	void print_tree() {
+        print_tree_help(_root, "");
+        std::cout << std::endl;
+    }
+
+    void print_tree_help(BSTNode<T>* n, std::string spaces) {
+        if(n != NULL){
+            print_tree_help(n->getRight(), spaces + "  ");
+            std::cout << spaces << n->getInfo() << std::endl;
+            print_tree_help(n->getLeft(), spaces + "  ");
+        }
+    }
 
 	/*
 	 return root 
--- C++/TestBSTCpp.cpp
+++ C++/TestBSTCpp.cpp
@@ -11,9 +11,22 @@ int main() {
 	std::cout << "Printing root, should be 5: " << myTree.get_root()->getInfo() << std::endl;
 	std::cout << "Printing isPresent(5), should be true: " << myTree.is_present(5) << std::endl;
 	myTree.insert(2);
+	myTree.print_tree();
 	myTree.insert(8);
+	myTree.insert(19);
+	myTree.insert(1);
+	myTree.print_tree();
+
 	std::cout << "Printing isPresent(7), should be false: " << myTree.is_present(7) << std::endl;
-	std::cout << "Printing isPresent(8), should be true: " << myTree.is_present(8) << std::endl;
+	std::cout << "Pinting isPresent(8), should be true: " << myTree.is_present(8) << std::endl;
+	myTree.remove(8);
+	myTree.remove(19);
+	myTree.remove(2);
+
+
+	myTree.print_tree();
+
+	std::cout << "Printing isPresent(8), should be false: " << myTree.is_present(8) << std::endl;
 
 
 	return 0;
